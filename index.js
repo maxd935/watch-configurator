6547756ae12d420dc557131e8d5a967d8c0dee83
Andre Sbrocco Figueiredo <andresbrocco@gmail.com>
2021-07-22 11:13:32 +0200

    testing deployment with heroku-buildpack-static

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const watch_options = require('./options.json')
 
-app.get('/watch_options', (req,res) => {
+app.get('/', (req,res) => {
     res.status(200).json(watch_options);
 })
 
