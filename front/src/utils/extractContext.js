7a7ddbf118a5ce65147441a612ad132314dbeeed
Jonathan RIVO <rivo.jonathan98@gmail.com>
2021-07-22 17:16:40 +0200

    Added ?

--- front/src/utils/extractContext.js
+++ front/src/utils/extractContext.js
@@ -3,24 +3,24 @@ import { ConfiguratorContext } from "../context/configuratorContext";
 
 export default function ExtractContext() {
   const [context, _] = useContext(ConfiguratorContext);
-  let collection = context.collection.options[context.collection.selected];
-  let dial = context.dial.options[context.dial.selected];
-  let material = context.strap.options[context.strap.selected].material;
-  let size = context.size.options[context.size.selected];
+  let collection = context?.collection?.options[context?.collection?.selected];
+  let dial = context?.dial?.options[context?.dial?.selected];
+  let material = context?.strap?.options[context?.strap?.selected].material;
+  let size = context?.size?.options[context?.size?.selected];
   let style =
-    context.strap.options[context.strap.selected].style.options[
-      context.strap.options[context.strap.selected].style.selected
+    context?.strap?.options[context?.strap?.selected].style?.options[
+      context?.strap?.options[context?.strap?.selected].style?.selected
     ];
   let color =
-  context.strap.options[context.strap.selected].color.options[
-    context.strap.options[context.strap.selected].color.selected
-  ]
-//   let housingName;
-//   let buckleName;
-//   let dial;
-//   let needles;
+    context?.strap?.options[context?.strap?.selected].color?.options[
+      context?.strap?.options[context?.strap?.selected].color?.selected
+    ];
+  //   let housingName;
+  //   let buckleName;
+  //   let dial;
+  //   let needles;
 
-//   let output = {housing}
+  //   let output = {housing}
 
   return { collection, dial, material, size, style, color };
 }
