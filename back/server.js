ed9911173614536d351e728d306106c9b644d5f2
Andre Sbrocco Figueiredo <andresbrocco@gmail.com>
2021-07-22 15:50:32 +0200

    all-in-one server (back and front)

--- back/server.js
+++ back/server.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
 const watch_options = require('./options.json')
+const path = require('path');
+const buildPath = path.join(__dirname, '..', 'build');
 
-express()
-    .get('/',
-        (req, res) =>
-            res.send('hello maxime')
-    )
-    .get('/watch_options', (req,res) => {
-        res.status(200).json(watch_options);
-    })
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+app
+	.get('/watch_options', (req,res) => {
+		res.status(200).json(watch_options);
+	})
+	.use(express.static(buildPath))
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`))
